41af6b36dac60649a28fcf9b25af99c1930e3c50
sharim shaikh <109298437+Sharim0684@users.noreply.github.com>
2024-08-08 11:53:00 +0000

    Compare

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);  //to give a correct o/p give a same datatypes
+// console.log("02" > 1);
+
+console.log(null > 0)
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0); // it gives always false value
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//strict check 
+// ===
+
+console.log("2" === 2);
+console.log(2 === 2);
+
