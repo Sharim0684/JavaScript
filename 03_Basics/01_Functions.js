acdad5ba7036df86cb75ee123d82fb7b8b029447
sharim shaikh <109298437+Sharim0684@users.noreply.github.com>
2024-08-14 04:33:20 +0000

    Function and Arrays

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -33,9 +33,52 @@ function addTwoNumbers(number1, number2)
 const result = addTwoNumbers(3,5)
 //console.log("result",result);
 
-function loginUserMessage(username)
+function loginUserMessage(username = "Sam")  // default value is Sam
 {
+    if(!username) //OR if(username === undefined)
+    {
+      console.log("Please enter a username");
+      return 
+    }
     return `${username} just logged in `
 }
 
-console.log(loginUserMessage("Sharim"));
+//console.log(loginUserMessage("Sharim"));
+
+//console.log(loginUserMessage("Sharim"));  // undefined just logged in
+
+function calculateCartPrice(val1, val2, ...num1)// ...(three Dots) this is an rest operate and also known as spread operator
+{
+  return num1
+}
+
+//console.log(calculateCartPrice(2));
+//console.log(calculateCartPrice(200,400,500,2000));  // it means that val1 = 200, val2 = 400 and rest of the value in num1 that why we used rest operator
+
+const user = {
+  username:"Sharim",
+  price: 199
+}
+
+function handleObject(anyobject)
+{
+  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+/*
+handleObject({
+  username:"Sam",
+  price:399    // we can pass an object directly also when we call the function 
+})  */
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray)
+{
+  return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,400,500,3000]));
