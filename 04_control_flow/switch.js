5f1a2ba11d6df6a75f74645f6f485310726e0439
sharim shaikh <109298437+Sharim0684@users.noreply.github.com>
2024-08-15 12:48:21 +0000

    Switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+/*
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+*/
+
+// const month = 3
+// switch(month)
+// {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     default:
+//         console.log("Default case match");      
+//         break;  
+// }
+
+
+const month = "march"
+switch(month)
+{
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
